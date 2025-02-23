bace96e8e58a637af211b2f1d985d40bd918b072
AurLemon <aurlemoncn@gmail.com>
2025-02-24 02:51:52 +0800

    refactor: 使用 Fastify 重构结构

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+"use strict";
 
-app.get('/', (req, res) => {
-    res.send('Express Test');
-});
+const fs = require("fs");
+const path = require("node:path");
+const AutoLoad = require("@fastify/autoload");
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+const options = {};
+
+module.exports = async function (fastify, opts) {
+  // 自动加载插件
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
+
+  // 自动加载路由
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "routes"),
+    options: {
+      prefix: "/api",
+    },
+  });
+
+  // 注册静态文件服务
+  fastify.register(require("@fastify/static"), {
+    root: path.join(__dirname, "public"),
+    prefix: "/",
+  });
+
+  // 捕获所有未匹配的请求
+  fastify.setNotFoundHandler(async (request, reply) => {
+    const filePath = path.join(__dirname, "public", request.raw.url);
+    try {
+      await fs.promises.access(filePath, fs.constants.F_OK);
+      return reply.sendFile(request.raw.url.substring(1));
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        return reply.sendFile("index.html");
+      } else {
+        reply.code(500).send(err);
+      }
+    }
+  });
+};
+
+module.exports.options = options;
