1d4539ea4e1692e40448e85646ad253f6b54ef56
AurLemon <aurlemoncn@gmail.com>
2025-03-05 15:17:19 +0800

    feat(courseService): 添加生成课程表方法及其对应的单元测试

--- services/courseService.js
+++ services/courseService.js
@@ -1,2 +1,35 @@
 // services/courseService.js
 'use strict';
+
+const scheduleService = require('./scheduleService');
+const api = require('../utils/api');
+
+const axiosIPv4 = api.axiosIPv4();
+const getAllCourses = async (userToken, studentId, semester) => {
+  if (!Array.isArray(semester)) {
+    throw new Error("Semester info must be an array.");
+  }
+
+  if (!studentId || !userToken) {
+    throw new Error("Student ID or User token must be provided.");
+  }
+
+  const coursesMap = new Map();
+  
+  const promises = semester.map(async (week) => {
+    try {
+      console.log("Requesting week: " + week.week + ". Total: " + semester.length);
+      const weekCourseData = await scheduleService.getWeekCourse(userToken, studentId, week.start_time);
+      coursesMap.set(week.week, weekCourseData);
+      console.log("Request finished for week:", week.week);
+    } catch (error) {
+      console.error(`Failed to fetch data for week ${week.week}:`, error);
+    }
+  });
+  
+  await Promise.all(promises);
+  
+  return coursesMap;
+};
+
+module.exports = { getAllCourses };
--- test/services/courseService.test.js
+++ test/services/courseService.test.js
@@ -1,2 +1,31 @@
 // test/services/courseService.test.js
 'use strict';
+
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
+const courseService = require('../../services/courseService');
+const scheduleService = require('../../services/scheduleService');
+const authService = require('../../services/authService');
+
+const TEST_UCODE = process.env.TEST_STUDENT_UCODE;
+
+(async () => {
+  const userInfo = await authService.getUserInfo(TEST_UCODE);
+
+  try {
+    const schoolYear = await scheduleService.getSchoolYear(userInfo.accessToken);
+    const currentSemester = schoolYear.findLast(s => s.is_current_semester);
+    if (!currentSemester) return;
+    const semester = await scheduleService.getSemester(userInfo.accessToken, currentSemester.school_year, currentSemester.semester);
+    
+    const allCourses = await courseService.getAllCourses(userInfo.accessToken, userInfo.studentId, semester);
+    console.log("All courses:", JSON.stringify(Object.fromEntries([...allCourses.entries()].sort(([a], [b]) => a - b)), null, 2));
+
+    fs.writeFileSync(path.join(__dirname, '../data/all_courses.json'), JSON.stringify(Object.fromEntries([...allCourses.entries()].sort(([a, b]) => a - b)), null, 2), 'utf8');
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
+})();
